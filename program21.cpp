9b611498a496d69c5c0dbe546bcd2e42bdbfbbc5
Satya Putra Wijaya <satya.825250027@stu.untar.ac.id>
2026-03-26 08:45:53 +0700

    Add files via upload

---
+++ program21.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int n,flag=1;
+    scanf("%d",&n);
+    for(int i=2;i<n;i++)
+        if(n%i==0) flag=0;
+    if(flag) printf("Prima\n");
+}
---
+++ program24.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    int a[5];
+    for(int i=0;i<5;i++)
+        scanf("%d",&a[i]);
+}
